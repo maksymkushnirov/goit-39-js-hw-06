661f8081151642046fbd24c9032f8bd18b926c38
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-06 13:05:52 +0200

    solve task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -13,6 +13,22 @@
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const bodyEl = document.querySelector('body');
+const textEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+
+buttonEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+    textEl.textContent = getRandomRGBColor();
+    bodyEl.style.backgroundColor = getRandomRGBColor();
+}
+
+function getRandomIntegerFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function getRandomRGBColor() {
+    return `rgb(${getRandomIntegerFromInterval(0, 255)},
+    ${getRandomIntegerFromInterval(0, 255)},
+    ${getRandomIntegerFromInterval(0, 255)})`;
 }
