0714b1fc808ef656c44bc35b1d0aee9dfd44bc3b
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-06 11:37:06 +0200

    solve task-05

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -4,3 +4,12 @@
 
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const inputEl = document.querySelector('#name-input');
+const nameInTitleEl = document.querySelector('#name-output');
+inputEl.addEventListener('input', event => {
+    nameInTitleEl.textContent = event.currentTarget.value;
+    if (event.currentTarget.value.trim() === '') {
+        nameInTitleEl.textContent = 'Anonymous';
+    }
+});
