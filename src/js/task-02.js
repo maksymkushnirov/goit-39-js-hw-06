b2cf434d0a12c6135860d91521df537023cffd57
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-11-06 09:55:53 +0200

    solve task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -29,3 +29,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const arrayItemsEl = ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.classList.add('item');
+    item.textContent = `${ingredient}`;
+    return item;
+});
+
+ingredientsEl.append(...arrayItemsEl);
+
+console.log(arrayItemsEl);
