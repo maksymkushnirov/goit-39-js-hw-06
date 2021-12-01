7fe5c1cccbe3a1a0178b804230d2e432b4a8c462
Maksym Kushnirov <maksymkushnirov@gmail.com>
2021-12-01 23:08:07 +0200

    task-08 login-form option-02

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -20,26 +20,48 @@
 // 4. Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // 5. Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+// Option 1
+
+// const formEl = document.querySelector(".login-form");
+
+// formEl.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+
+//   const email = formElements.email.value;
+//   const password = formElements.password.value;
+
+//   if (email === "" || password === "") {
+//     alert("Все поля должны быть заполнены");
+//   } else {
+//     const formData = {
+//       email,
+//       password,
+//     };
+
+//     console.log(formData);
+//     formEl.reset();
+//   }
+// }
+
+// Option 2
+
 const formEl = document.querySelector(".login-form");
 
 formEl.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const formElements = event.currentTarget.elements;
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    if (value === "") {
+      alert("Все поля должны быть заполнены");
+    } else {
+      console.log(`${name}: ${value}`);
+    }
+  });
 
-  const email = formElements.email.value;
-  const password = formElements.password.value;
-
-  if (email === "" || password === "") {
-    alert("Все поля должны быть заполнены");
-  } else {
-    const formData = {
-      email,
-      password,
-    };
-
-    console.log(formData);
-    formEl.reset();
-  }
+  formEl.reset();
 }
